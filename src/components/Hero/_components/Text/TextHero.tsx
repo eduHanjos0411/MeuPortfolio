6e39a81369c238cad3d725184f79330e1eee2a1c
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:26:19 -0300

    feat: creating component fo hero text

---
+++ src/components/Hero/_components/Text/TextHero.tsx
@@ -0,0 +1,15 @@
+import "./textHero.css"
+
+export function TextHero() {
+  return (
+    <p className="text-hero">
+      Desenvolvedor Fullstack com foco em <strong>Java e Spring</strong> no
+      back-end e <strong> Typescript e React </strong> no front-end, criando
+      aplicações web bem estruturadas e funcionais. Trabalho com desenvolvimento
+      de <strong>APIs REST</strong>, <strong>banco de dados relacionais</strong>{" "}
+      e interface modernas reutilizáveis, sempre buscando a{" "}
+      <strong>excelência</strong>, <strong>organização</strong> e{" "}
+      <strong>eficiência</strong>.
+    </p>
+  )
+}
