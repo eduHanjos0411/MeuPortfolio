e337a97e3bed5c4f99690eed02a8e3b80d86c464
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:32:18 -0300

    feat: creating about section component

---
+++ src/components/About/index.tsx
@@ -0,0 +1,14 @@
+import { TitleAbout } from "./_components/Title/TitleAbout";
+import './about.css'
+
+export function About() {
+  return (
+    <main className="about-section">
+    <TitleAbout/>
+    <div>
+    {/* <TextAbout/> */}
+    <img src=""/>
+    </div>
+    </main>
+  )
+}
