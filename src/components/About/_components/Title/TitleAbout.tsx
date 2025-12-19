ecfcf706d95e15323171a3520e3acea06a4a8a11
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:31:55 -0300

    feat: creating component for about section title

---
+++ src/components/About/_components/Title/TitleAbout.tsx
@@ -0,0 +1,9 @@
+import './titleAbout.css'
+
+export function TitleAbout() {
+  return (
+    <div className="title-about-wrapper">
+      <h1 className="title-about">Quem sou eu?</h1>
+    </div>
+  );
+}
