9b90f9bd19f3f10c18818834ad425151a479eb06
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:32:33 -0300

    feat: adding more section to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css"
+import { About } from "./components/About"
 import { BackgroundParticles } from "./components/BackgroundParticles/BackgroundParticles"
 import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
 
 function App() {
   return <>
   <BackgroundParticles/>
   <Header/>
+  <Hero/>
+  <About/>
   </>
 }
 
