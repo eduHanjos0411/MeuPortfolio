12faa02bbaea0000ed8a01aa90ca2499b72ae9de
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:26:53 -0300

    remove: removing not used component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,12 @@
+import { TextHero } from "./_components/Text/TextHero";
+import { TitleHero } from "./_components/Title/TitleHero";
+import './hero.css'
+
 export function Hero() {
   return (
-    <main>
+    <main className="hero-section">
       <TitleHero/>
       <TextHero/>
-      <CTAButton/>
     </main>
   )
 }
