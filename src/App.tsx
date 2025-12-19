f34c02a3058a7748d3e8309ad167c4b157bd7c4c
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 11:27:47 -0300

    feat: adding components to display

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { BackgroundParticles } from "./components/BackgroundParticles/BackgroundParticles"
+import { Header } from "./components/Header"
+
+function App() {
+  return <>
+  <BackgroundParticles/>
+  <Header/>
+  </>
+}
+
+export default App
