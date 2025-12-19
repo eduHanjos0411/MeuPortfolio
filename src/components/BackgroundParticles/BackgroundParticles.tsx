457abad01fc3a6d5aa920982ac1fc8353df95a6c
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-19 12:28:21 -0300

    refactor: correctly applying useParticle hook

--- src/components/BackgroundParticles/BackgroundParticles.tsx
+++ src/components/BackgroundParticles/BackgroundParticles.tsx
@@ -1,5 +1,8 @@
+import { useParticle } from '../../hooks/useParticles'
 import './background.css'
 
 export function BackgroundParticles() {
+    useParticle()
+
   return <canvas id="particles-canvas" />
 }
