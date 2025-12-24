ef7333a3e698bb3db4df9f843d8ecde124679fd2
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-24 11:15:47 -0300

    feat: adding id for navigation and displaying children component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,12 +1,13 @@
+import { TextAbout } from "./_components/Text/TextAbout";
 import { TitleAbout } from "./_components/Title/TitleAbout";
 import './about.css'
 
 export function About() {
   return (
-    <main className="about-section">
+    <main className="about-section" id="about">
     <TitleAbout/>
     <div>
-    {/* <TextAbout/> */}
+    <TextAbout/> 
     <img src=""/>
     </div>
     </main>
