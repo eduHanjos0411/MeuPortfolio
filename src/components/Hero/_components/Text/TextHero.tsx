1d45c015af8c1461e6e82eb1725d46a21c3aaecf
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2025-12-24 11:17:25 -0300

    refactor: improving readablity of text displayed

--- src/components/Hero/_components/Text/TextHero.tsx
+++ src/components/Hero/_components/Text/TextHero.tsx
@@ -3,7 +3,7 @@ import "./textHero.css"
 export function TextHero() {
   return (
     <p className="text-hero">
-      Desenvolvedor Fullstack com foco em <strong>Java e Spring</strong> no
+      Dev. Fullstack com foco em <strong>Java e Spring</strong> no
       back-end e <strong> Typescript e React </strong> no front-end, criando
       aplicações web bem estruturadas e funcionais. Trabalho com desenvolvimento
       de <strong>APIs REST</strong>, <strong>banco de dados relacionais</strong>{" "}
