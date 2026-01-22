12124c15687cee64c1c41184dae30b8e4b69d6b8
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-01-22 11:23:49 -0300

    feat: adding projects title component

---
+++ src/components/Projects/_components/Title/TitleProjects.tsx
@@ -0,0 +1,9 @@
+import './titleProjects.css'
+
+export function TitleProjects() {
+  return (
+    <div className="title-projects-wrapper">
+      <h1 className="title-projects">Meus projetos</h1>
+    </div>
+  )
+}
