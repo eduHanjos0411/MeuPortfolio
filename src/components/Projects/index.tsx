329cd653d32c8cd3f60e0f8e9389565f196bc252
eduHanjos0411 <eduardo.h.anjos0411@gmail.com>
2026-01-22 11:23:33 -0300

    feat: creating projects section and projects display

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,37 @@
+import { ProjectCard, type CardProps } from "./_components/ProjectCard/ProjectCard"
+import { SubtitleProjects } from "./_components/Subtitle/SubtitleProjects"
+import { TitleProjects } from "./_components/Title/TitleProjects"
+
+import './projects.css'
+
+const cardInfos: CardProps[] = [
+  {
+    title: "Aprimorar",
+    description: "Landing page desenvolvida para um ambiente de reforço escolar e preparativos para vestibular",
+    srcImage: "mockupAprimorar.png"
+  },
+  // {
+  //   title: "",
+  //   description: ""
+  // },
+  //   {
+  //   title: "",
+  //   description: ""
+  // },
+]
+
+export function Projects() {
+  return (
+    <>
+      <main className="projects-section">
+        <TitleProjects />
+        <SubtitleProjects/>
+        <div>
+          {cardInfos.map((card, idx) => (
+            <ProjectCard key={idx} {...card}/>
+          ))}
+        </div>
+      </main>
+    </>
+  )
+}
